6f1e3e1a00a5c4e61029787e4ff26ed25bac3993
LinTan <zuilintan@163.com>
2020-06-22 15:45:28 +0800

    BaseViewHolder: 1, 添加图片着色方法 2, 移除无用方法, 简化属性设置流程

--- library/src/main/java/com/lt/library/base/BaseViewHolder.java
+++ library/src/main/java/com/lt/library/base/BaseViewHolder.java
@@ -1,14 +1,18 @@
 package com.lt.library.base;
 
+import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.support.annotation.ColorRes;
+import android.support.annotation.DimenRes;
 import android.support.annotation.DrawableRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.StringRes;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.graphics.drawable.DrawableCompat;
 import android.support.v7.widget.RecyclerView;
 import android.util.SparseArray;
+import android.util.TypedValue;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -29,6 +33,7 @@ import java.util.Objects;
  */
 
 public class BaseViewHolder extends RecyclerView.ViewHolder {
+    public static final int RES_EMPTY_ID = 0;
     private SparseArray<View> mSparseArray;//Key为View的Id，Value为View对象
 
     public BaseViewHolder(@NonNull View itemView) {
@@ -46,6 +51,10 @@ public class BaseViewHolder extends RecyclerView.ViewHolder {
         return (T) view;
     }//findViewById，并复用
 
+    public Context getContext() {
+        return ContextUtil.getInstance().getContext();
+    }
+
     public void setText(int viewId, CharSequence text) {
         TextView view = findViewById(viewId);
         view.setText(text);
@@ -61,50 +70,66 @@ public class BaseViewHolder extends RecyclerView.ViewHolder {
         return view.getText().toString();
     }//获取文本
 
-    public void setTextSize(int viewId, float size) {
-        TextView view = findViewById(viewId);
-        view.setTextSize(size);
+    public void setTextSize(int viewId, @DimenRes int dimenId) {
+        setTextSize(viewId, dimenId, TypedValue.COMPLEX_UNIT_SP);
     }//设置文本大小
 
-    public void setTextColor(int viewId, @ColorRes int resId) {
+    public void setTextSize(int viewId, @DimenRes int dimenId, int unit) {
         TextView view = findViewById(viewId);
-        view.setTextColor(ContextCompat.getColor(ContextUtil.getInstance().getContext(), resId));
+        view.setTextSize(unit, getContext().getResources().getDimension(dimenId));
+    }//设置文本大小
+
+    public void setTextColor(int viewId, @ColorRes int colorId) {
+        TextView view = findViewById(viewId);
+        view.setTextColor(ContextCompat.getColor(getContext(), colorId));
     }//设置文本颜色
 
-    public void setTextColorStateList(int viewId, @ColorRes int resId) {
+    public void setTextColorStateList(int viewId, @ColorRes int colorId) {
         TextView view = findViewById(viewId);
-        view.setTextColor(ContextCompat.getColorStateList(ContextUtil.getInstance().getContext(), resId));
+        view.setTextColor(ContextCompat.getColorStateList(getContext(), colorId));
     }//设置文本颜色
 
-    public void setImageDrawable(int viewId, @Nullable Drawable drawable) {
+    public void setImageDrawable(int viewId, @DrawableRes int drawableId) {
         ImageView view = findViewById(viewId);
+        if (drawableId == RES_EMPTY_ID) {
+            view.setImageDrawable(null);
+        } else {
+            view.setImageDrawable(ContextCompat.getDrawable(getContext(), drawableId));
+        }
+    }//设置图片资源
+
+    public void setImageDrawableTint(int viewId, @DrawableRes int drawableId, @ColorRes int colorId) {
+        ImageView view = findViewById(viewId);
+        Drawable drawable = ContextCompat.getDrawable(getContext(), drawableId);
+        DrawableCompat.setTint(Objects.requireNonNull(drawable), ContextCompat.getColor(getContext(), colorId));
         view.setImageDrawable(drawable);
     }//设置图片资源
 
-    public void setImageResource(int viewId, @DrawableRes int resId) {
+    public void setImageDrawableTintList(int viewId, @DrawableRes int drawableId, @ColorRes int colorId) {
         ImageView view = findViewById(viewId);
-        view.setImageResource(resId);
+        Drawable drawable = ContextCompat.getDrawable(getContext(), drawableId);
+        DrawableCompat.setTintList(Objects.requireNonNull(drawable), ContextCompat.getColorStateList(getContext(), colorId));
+        view.setImageDrawable(drawable);
     }//设置图片资源
 
-    public void setBackground(int viewId, @Nullable Drawable drawable) {
+    public void setBackground(int viewId, @DrawableRes int drawableId) {
         View view = findViewById(viewId);
-        view.setBackground(drawable);
+        if (drawableId == RES_EMPTY_ID) {
+            view.setBackground(null);
+        } else {
+            view.setBackground(ContextCompat.getDrawable(getContext(), drawableId));
+        }
     }//设置背景资源
 
-    public void setBackgroundResource(int viewId, @DrawableRes int resId) {
+    public void setBackgroundColor(int viewId, @ColorRes int colorId) {
         View view = findViewById(viewId);
-        view.setBackgroundResource(resId);
-    }//设置背景资源
-
-    public void setBackgroundColor(int viewId, @ColorRes int resId) {
-        View view = findViewById(viewId);
-        view.setBackgroundColor(ContextCompat.getColor(ContextUtil.getInstance().getContext(), resId));
+        view.setBackgroundColor(ContextCompat.getColor(getContext(), colorId));
     }//设置背景颜色
 
     public void setVisibility(int viewId, int visibility) {
         View view = findViewById(viewId);
         view.setVisibility(visibility);
-    }//设置显隐
+    }//设置显隐状态
 
     public void setOnClickListener(int viewId, @Nullable View.OnClickListener listener) {
         View view = findViewById(viewId);
