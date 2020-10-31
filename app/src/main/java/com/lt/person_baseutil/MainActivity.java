6fd817c78d06fd7eb0dedbef7c0265aefd67bb76
zuilintan <zuilintan@163.com>
2020-10-31 14:59:20 +0800

    App 1, 测试, LogUtil输出效果 2, 添加, HunterDebug

--- app/src/main/java/com/lt/person_baseutil/MainActivity.java
+++ app/src/main/java/com/lt/person_baseutil/MainActivity.java
@@ -1,13 +1,39 @@
 package com.lt.person_baseutil;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.hunter.library.debug.HunterDebugImpl;
+import com.hunter.library.debug.HunterLoggerHandler;
+import com.lt.library.util.LogUtil;
 
 public class MainActivity extends AppCompatActivity {
 
+    public MainActivity() {
+        HunterLoggerHandler.installLogImpl(new HunterLoggerHandler() {
+            @Override
+            protected void log(String tag, String msg) {
+                LogUtil.i(tag, msg);
+            }
+        });
+    }
+
+    @HunterDebugImpl
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LogUtil.v("msgV");
+        LogUtil.v("tagV", "msgV");
+        LogUtil.d("msgD");
+        LogUtil.d("tagD", "msgD");
+        LogUtil.i("msgI");
+        LogUtil.i("tagI", "msgI");
+        LogUtil.w("msgW");
+        LogUtil.w("tagW", "msgW");
+        LogUtil.e("msgE");
+        LogUtil.e("tagE", "msgE");
+        LogUtil.wtf("msgWtf");
+        LogUtil.wtf("tagWtf", "msgWtf");
     }
 }
