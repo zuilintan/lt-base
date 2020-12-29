24dab25b5a195c37c2bfed8d07e29dda5c2d45b9
LinTan <zuilintan@163.com>
2020-12-30 01:28:23 +0800

    App: 1, 完善, 测试Demo

--- app/src/main/java/com/lt/person_baseutil/view/adapter/TestAdapter.java
+++ app/src/main/java/com/lt/person_baseutil/view/adapter/TestAdapter.java
@@ -1,23 +1,51 @@
 package com.lt.person_baseutil.view.adapter;
 
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.widget.CompoundButton;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
 import com.kyleduo.switchbutton.SwitchButton;
 import com.lt.library.base.recyclerview.adapter.BaseAdapter;
+import com.lt.library.base.recyclerview.viewholder.BaseViewHolder;
 import com.lt.library.base.recyclerview.viewholder.EntityViewHolder;
+import com.lt.library.util.LogUtil;
+import com.lt.library.util.ToastUtil;
 import com.lt.person_baseutil.R;
 import com.lt.person_baseutil.model.entity.TestBean;
 
 import java.util.List;
 
 public class TestAdapter extends BaseAdapter<TestBean> {
+    private CompoundButton.OnCheckedChangeListener mOnCheckedChangeListener = (buttonView, isChecked) -> {
+        ToastUtil.show("tag = " + buttonView.getTag() + ", isChecked = " + isChecked + ", isPressed = " + buttonView.isPressed());
+    };
+    private SeekBar.OnSeekBarChangeListener mOnSeekBarChangeListener = new SeekBar.OnSeekBarChangeListener() {
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+            ToastUtil.show("tag = " + seekBar.getTag() + ", progress = " + progress + ", isFromUser = " + fromUser);
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+        }
+    };
 
     public TestAdapter() {
     }
 
     public TestAdapter(List<TestBean> entityList) {
         super(entityList);
+    }
+
+    @Override
+    protected int getEntityViewType(int position) {
+        return getEntity(position).getItemType();
     }
 
     @Override
@@ -53,17 +81,49 @@ public class TestAdapter extends BaseAdapter<TestBean> {
                 viewHolder.setImageDrawable(R.id.iv_item_ico, R.drawable.ic_fire);
                 SwitchButton switchButton = viewHolder.findViewById(R.id.sBtn_item_switch);
                 switchButton.setChecked(Boolean.parseBoolean(dataSource.getItemValue()));
+                if ((position & 1) == 1) {
+                    switchButton.setTag("sBtn_tag");
+                } else {
+                    switchButton.setTag(null);
+                }
+                switchButton.setOnCheckedChangeListener(mOnCheckedChangeListener);
                 break;
             case TestBean.ITEM_TYPE_SLIDE:
                 viewHolder.setImageDrawable(R.id.iv_item_ico, R.drawable.ic_toy);
                 SeekBar seekBar = viewHolder.findViewById(R.id.sb_item_slide);
                 seekBar.setProgress(Integer.parseInt(dataSource.getItemValue()));
+                if ((position & 1) == 1) {
+                    seekBar.setTag("sb_tag");
+                } else {
+                    seekBar.setTag(null);
+                }
+                seekBar.setOnSeekBarChangeListener(mOnSeekBarChangeListener);
                 break;
         }
     }
 
     @Override
-    protected int getEntityViewType(int position) {
-        return getEntity(position).getItemType();
+    protected void onRecycledView(BaseViewHolder viewHolder, int viewType) {
+        super.onRecycledView(viewHolder, viewType);
+        switch (viewType) {
+            case TestBean.ITEM_TYPE_SWITCH:
+                SwitchButton switchButton = viewHolder.findViewById(R.id.sBtn_item_switch);
+                switchButton.setOnCheckedChangeListener(null);
+                break;
+            case TestBean.ITEM_TYPE_SLIDE:
+                SeekBar seekBar = viewHolder.findViewById(R.id.sb_item_slide);
+                seekBar.setOnSeekBarChangeListener(null);
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onDetachedFromRecyclerView(@NonNull RecyclerView recyclerView) {
+        super.onDetachedFromRecyclerView(recyclerView);
+        LogUtil.w("GG");
+        mOnCheckedChangeListener = null;
+        mOnSeekBarChangeListener = null;
     }
 }
