e682707b93566bd4c260faff45c0347b17370f7f
LinTan <zuilintan@163.com>
2021-01-04 09:43:34 +0800

    BaseDialogFragment 1, 添加, 按钮点击事件

---
+++ library/src/main/java/com/lt/library/base/dialogfragment/listenter/OnNegativeButtonClickListener.java
@@ -0,0 +1,7 @@
+package com.lt.library.base.dialogfragment.listenter;
+
+import android.view.View;
+
+public interface OnNegativeButtonClickListener {
+     void onNegativeButtonClick(View view);
+}
