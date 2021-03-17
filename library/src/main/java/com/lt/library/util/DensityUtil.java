f856a6ea00b1e3b941fffa6abb4a1a36fe90b265
LinTan <zuilintan@163.com>
2021-03-18 00:42:32 +0800

    Project: 1, Opt, Code

--- library/src/main/java/com/lt/library/util/DensityUtil.java
+++ library/src/main/java/com/lt/library/util/DensityUtil.java
@@ -14,6 +14,7 @@ import com.lt.library.util.context.ContextUtil;
  */
 
 public class DensityUtil {
+
     private DensityUtil() {
         throw new UnsupportedOperationException("cannot be instantiated");
     }
