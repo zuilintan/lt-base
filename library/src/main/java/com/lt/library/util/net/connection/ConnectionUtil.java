d1d18f40080e7010bf950880c65a03c758f1cfd0
LinTan <zuilintan@163.com>
2021-03-31 11:53:10 +0800

    ConnectionUtil: 1, Opt, Code, 使用构造者模式

--- library/src/main/java/com/lt/library/util/net/connection/ConnectionUtil.java
+++ library/src/main/java/com/lt/library/util/net/connection/ConnectionUtil.java
@@ -7,7 +7,6 @@ import android.net.Network;
 import android.net.NetworkCapabilities;
 import android.os.Handler;
 import android.os.Looper;
-import android.os.Message;
 import android.support.annotation.IntDef;
 
 import com.lt.library.util.LogUtil;
@@ -31,48 +30,125 @@ public class ConnectionUtil {
     private OnNetworkConnectionListener mOnNetworkConnectionListener;
     private OnCellularNetworkConnectionListener mOnCellularNetworkConnectionListener;
     private OnWifiNetworkConnectionListener mOnWifiNetworkConnectionListener;
-    private final Map<String, Integer> mNetworkTransportMap = new ConcurrentHashMap<>();
-    private final Handler mHandler = new Handler(Looper.getMainLooper(), this::handleMessage);
-    private final ConnectivityManager.NetworkCallback mNetworkCallback = new ConnectivityManager.NetworkCallback() {
-        @Override
-        public void onAvailable(Network network) {
-            super.onAvailable(network);
-            LogUtil.d("network = " + network);
-            notifyNetworkAvailable(network);
-        }//当网络连接成功, 可以使用时
+    private final Handler mHandler;
+    private final Map<String, Integer> mNetworkTransportMap;
+    private final ConnectivityManager.NetworkCallback mNetworkCallback;
 
-        @Override
-        public void onLosing(Network network, int maxMsToLive) {
-            super.onLosing(network, maxMsToLive);
-            LogUtil.d("network = " + network + ", maxMsToLive = " + maxMsToLive);
-        }//当网络连接正在断开
+    private ConnectionUtil() {
+        mHandler = new Handler(Looper.getMainLooper(), msg -> {
+            switch (msg.what) {
+                case HANDLE_WHAT_NETWORK_CONNECTION_LISTENER:
+                    callOnNetworkConnectionListener((Integer) msg.obj);
+                    break;
+                case HANDLE_WHAT_CELLULAR_CONNECTION_LISTENER:
+                    callOnCellularConnectionListener((Integer) msg.obj);
+                    break;
+                case HANDLE_WHAT_WIFI_CONNECTION_LISTENER:
+                    callOnWifiConnectionListener((Integer) msg.obj);
+                    break;
+                default:
+                    break;
+            }
+            return true;
+        });
+        mNetworkTransportMap = new ConcurrentHashMap<>();
+        mNetworkCallback = new ConnectivityManager.NetworkCallback() {
+            @Override
+            public void onAvailable(Network network) {
+                super.onAvailable(network);
+                LogUtil.d("network = " + network);
+                notifyNetworkAvailable(network);
+            }//当网络连接成功, 可以使用时
 
-        @Override
-        public void onLost(Network network) {
-            super.onLost(network);
-            LogUtil.d("network = " + network);
-            notifyNetworkLost(network);
-        }//当网络连接已经断开
+            @Override
+            public void onLosing(Network network, int maxMsToLive) {
+                super.onLosing(network, maxMsToLive);
+                LogUtil.d("network = " + network + ", maxMsToLive = " + maxMsToLive);
+            }//当网络连接正在断开
 
-        @Override
-        public void onUnavailable() {
-            super.onUnavailable();
-            LogUtil.d();
-        }//当网络连接超时或网络请求达不到可用要求
+            @Override
+            public void onLost(Network network) {
+                super.onLost(network);
+                LogUtil.d("network = " + network);
+                notifyNetworkLost(network);
+            }//当网络连接已经断开
 
-        @Override
-        public void onCapabilitiesChanged(Network network, NetworkCapabilities networkCapabilities) {
-            super.onCapabilitiesChanged(network, networkCapabilities);
-            LogUtil.d("network = " + network + ", networkCapabilities = " + networkCapabilities);
-            notifyNetworkCapabilitiesChanged(network, networkCapabilities);
-        }//当网络连接的状态变化, 但仍旧可用
+            @Override
+            public void onUnavailable() {
+                super.onUnavailable();
+                LogUtil.d();
+            }//当网络连接超时或网络请求达不到可用要求
 
-        @Override
-        public void onLinkPropertiesChanged(Network network, LinkProperties linkProperties) {
-            super.onLinkPropertiesChanged(network, linkProperties);
-            LogUtil.d("network = " + network + ", linkProperties = " + linkProperties);
-        }//当网络连接的属性变化
-    };
+            @Override
+            public void onCapabilitiesChanged(Network network, NetworkCapabilities networkCapabilities) {
+                super.onCapabilitiesChanged(network, networkCapabilities);
+                LogUtil.d("network = " + network + ", networkCapabilities = " + networkCapabilities);
+                notifyNetworkCapabilitiesChanged(network, networkCapabilities);
+            }//当网络连接的状态变化, 但仍旧可用
+
+            @Override
+            public void onLinkPropertiesChanged(Network network, LinkProperties linkProperties) {
+                super.onLinkPropertiesChanged(network, linkProperties);
+                LogUtil.d("network = " + network + ", linkProperties = " + linkProperties);
+            }//当网络连接的属性变化
+        };
+    }
+
+    public static boolean isActiveNetworkValidated() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) ContextUtil.getInstance().getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+        Network network = connectivityManager.getActiveNetwork();
+        NetworkCapabilities networkCapabilities = connectivityManager.getNetworkCapabilities(network);
+        LogUtil.d("networkCapabilities = " + networkCapabilities);
+        return networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) && networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED);
+    }
+
+    public static boolean isActiveNetworkNotMetered() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) ContextUtil.getInstance().getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+        Network network = connectivityManager.getActiveNetwork();
+        NetworkCapabilities networkCapabilities = connectivityManager.getNetworkCapabilities(network);
+        LogUtil.d("networkCapabilities = " + networkCapabilities);
+        return networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_NOT_METERED);
+    }
+
+    public static boolean isActiveNetworkBelongTransport(int transport) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) ContextUtil.getInstance().getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+        Network network = connectivityManager.getActiveNetwork();
+        NetworkCapabilities networkCapabilities = connectivityManager.getNetworkCapabilities(network);
+        LogUtil.d("networkCapabilities = " + networkCapabilities);
+        return networkCapabilities.hasCapability(transport);
+    }
+
+    public static int getActiveNetworkTransport() {
+        int result = -1;
+        ConnectivityManager connectivityManager = (ConnectivityManager) ContextUtil.getInstance().getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+        Network network = connectivityManager.getActiveNetwork();
+        NetworkCapabilities networkCapabilities = connectivityManager.getNetworkCapabilities(network);
+        if (networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
+            LogUtil.d("network = " + network + ", transport = TRANSPORT_CELLULAR");
+            result = NetworkCapabilities.TRANSPORT_CELLULAR;
+        } else if (networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
+            LogUtil.d("network = " + network + ", transport = TRANSPORT_WIFI");
+            result = NetworkCapabilities.TRANSPORT_WIFI;
+        } else if (networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_BLUETOOTH)) {
+            LogUtil.d("network = " + network + ", transport = TRANSPORT_BLUETOOTH");
+            result = NetworkCapabilities.TRANSPORT_BLUETOOTH;
+        } else if (networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)) {
+            LogUtil.d("network = " + network + ", transport = TRANSPORT_ETHERNET");
+            result = NetworkCapabilities.TRANSPORT_ETHERNET;
+        } else if (networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_VPN)) {
+            LogUtil.d("network = " + network + ", transport = TRANSPORT_VPN");
+            result = NetworkCapabilities.TRANSPORT_VPN;
+        } else if (networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI_AWARE)) {
+            LogUtil.d("network = " + network + ", transport = TRANSPORT_WIFI_AWARE");
+            result = NetworkCapabilities.TRANSPORT_WIFI_AWARE;
+        } else if (networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_LOWPAN)) {
+            LogUtil.d("network = " + network + ", transport = TRANSPORT_LOWPAN");
+            result = NetworkCapabilities.TRANSPORT_LOWPAN;
+        } else {
+            LogUtil.w("network = " + network + ", transport = unknown, networkCapabilities = " + networkCapabilities);
+        }
+        return result;
+    }
 
     private void notifyNetworkAvailable(Network network) {
         ConnectivityManager connectivityManager = (ConnectivityManager) ContextUtil.getInstance().getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
@@ -149,103 +225,54 @@ public class ConnectionUtil {
         }
     }
 
-    private boolean handleMessage(Message msg) {
-        switch (msg.what) {
-            case HANDLE_WHAT_NETWORK_CONNECTION_LISTENER:
-                callOnNetworkConnectionListener((Integer) msg.obj);
-                break;
-            case HANDLE_WHAT_CELLULAR_CONNECTION_LISTENER:
-                callOnCellularConnectionListener((Integer) msg.obj);
-                break;
-            case HANDLE_WHAT_WIFI_CONNECTION_LISTENER:
-                callOnWifiConnectionListener((Integer) msg.obj);
-                break;
-            default:
-                break;
-        }
-        return true;
-    }
-
-    public boolean isActiveNetworkValidated() {
-        ConnectivityManager connectivityManager = (ConnectivityManager) ContextUtil.getInstance().getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
-        Network network = connectivityManager.getActiveNetwork();
-        NetworkCapabilities networkCapabilities = connectivityManager.getNetworkCapabilities(network);
-        LogUtil.d("networkCapabilities = " + networkCapabilities);
-        return networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) && networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED);
-    }
-
-    public boolean isActiveNetworkNotMetered() {
-        ConnectivityManager connectivityManager = (ConnectivityManager) ContextUtil.getInstance().getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
-        Network network = connectivityManager.getActiveNetwork();
-        NetworkCapabilities networkCapabilities = connectivityManager.getNetworkCapabilities(network);
-        LogUtil.d("networkCapabilities = " + networkCapabilities);
-        return networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_NOT_METERED);
-    }
-
-    public boolean isActiveNetworkBelongTransport(int transport) {
-        ConnectivityManager connectivityManager = (ConnectivityManager) ContextUtil.getInstance().getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
-        Network network = connectivityManager.getActiveNetwork();
-        NetworkCapabilities networkCapabilities = connectivityManager.getNetworkCapabilities(network);
-        LogUtil.d("networkCapabilities = " + networkCapabilities);
-        return networkCapabilities.hasCapability(transport);
-    }
-
-    public int getActiveNetworkTransport() {
-        int result = -1;
-        ConnectivityManager connectivityManager = (ConnectivityManager) ContextUtil.getInstance().getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
-        Network network = connectivityManager.getActiveNetwork();
-        NetworkCapabilities networkCapabilities = connectivityManager.getNetworkCapabilities(network);
-        if (networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
-            LogUtil.d("network = " + network + ", transport = TRANSPORT_CELLULAR");
-            result = NetworkCapabilities.TRANSPORT_CELLULAR;
-        } else if (networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
-            LogUtil.d("network = " + network + ", transport = TRANSPORT_WIFI");
-            result = NetworkCapabilities.TRANSPORT_WIFI;
-        } else if (networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_BLUETOOTH)) {
-            LogUtil.d("network = " + network + ", transport = TRANSPORT_BLUETOOTH");
-            result = NetworkCapabilities.TRANSPORT_BLUETOOTH;
-        } else if (networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)) {
-            LogUtil.d("network = " + network + ", transport = TRANSPORT_ETHERNET");
-            result = NetworkCapabilities.TRANSPORT_ETHERNET;
-        } else if (networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_VPN)) {
-            LogUtil.d("network = " + network + ", transport = TRANSPORT_VPN");
-            result = NetworkCapabilities.TRANSPORT_VPN;
-        } else if (networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI_AWARE)) {
-            LogUtil.d("network = " + network + ", transport = TRANSPORT_WIFI_AWARE");
-            result = NetworkCapabilities.TRANSPORT_WIFI_AWARE;
-        } else if (networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_LOWPAN)) {
-            LogUtil.d("network = " + network + ", transport = TRANSPORT_LOWPAN");
-            result = NetworkCapabilities.TRANSPORT_LOWPAN;
-        } else {
-            LogUtil.w("network = " + network + ", transport = unknown, networkCapabilities = " + networkCapabilities);
-        }
-        return result;
-    }
-
-    public void addEvent() {
+    private void addEvent() {
         ConnectivityManager connectivityManager = (ConnectivityManager) ContextUtil.getInstance().getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
         connectivityManager.registerDefaultNetworkCallback(mNetworkCallback);
     }
 
-    public void delEvent() {
+    private void delEvent() {
         ConnectivityManager connectivityManager = (ConnectivityManager) ContextUtil.getInstance().getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
         connectivityManager.unregisterNetworkCallback(mNetworkCallback);
     }
 
-    public void setOnNetworkConnectionListener(OnNetworkConnectionListener onNetworkConnectionListener) {
-        mOnNetworkConnectionListener = onNetworkConnectionListener;
-    }
-
-    public void setOnCellularNetworkConnectionListener(OnCellularNetworkConnectionListener onCellularNetworkConnectionListener) {
-        mOnCellularNetworkConnectionListener = onCellularNetworkConnectionListener;
-    }
-
-    public void setOnWifiNetworkConnectionListener(OnWifiNetworkConnectionListener onWifiNetworkConnectionListener) {
-        mOnWifiNetworkConnectionListener = onWifiNetworkConnectionListener;
+    public void release() {
+        delEvent();
     }
 
     @IntDef({CONNECTION_STATUS_NOT_CONNECTED, CONNECTION_STATUS_CONNECTED, CONNECTION_STATUS_CONNECTED_VALIDATED})
     @Retention(RetentionPolicy.SOURCE)
     public @interface NetworkDef {
+    }
+
+    public static class Builder {
+        private final ConnectionUtil mConnectionUtil;
+
+        public Builder() {
+            mConnectionUtil = new ConnectionUtil();
+        }
+
+        public Builder setOnNetworkConnectionListener(OnNetworkConnectionListener onNetworkConnectionListener) {
+            mConnectionUtil.mOnNetworkConnectionListener = onNetworkConnectionListener;
+            return this;
+        }
+
+        public Builder setOnCellularNetworkConnectionListener(OnCellularNetworkConnectionListener onCellularNetworkConnectionListener) {
+            mConnectionUtil.mOnCellularNetworkConnectionListener = onCellularNetworkConnectionListener;
+            return this;
+        }
+
+        public Builder setOnWifiNetworkConnectionListener(OnWifiNetworkConnectionListener onWifiNetworkConnectionListener) {
+            mConnectionUtil.mOnWifiNetworkConnectionListener = onWifiNetworkConnectionListener;
+            return this;
+        }
+
+        public ConnectionUtil build() {
+            if (mConnectionUtil.mOnNetworkConnectionListener != null || mConnectionUtil.mOnCellularNetworkConnectionListener != null || mConnectionUtil.mOnWifiNetworkConnectionListener != null) {
+                mConnectionUtil.addEvent();
+            } else {
+                throw new NullPointerException("at least one setOnXxxNetworkConnectionListener() must be called");
+            }
+            return mConnectionUtil;
+        }
     }
 }
